eda1cecaa5756b882f1aaa426737ee009a6038f4
Sujana D <sujanadrko7@gmail.com>
2025-06-30 10:22:59 +0530

    Added db.properties and fixed connection issue

--- CarRentalSystem-CASESTUDY/app/main.py
+++ CarRentalSystem-CASESTUDY/app/main.py
@@ -99,5 +99,6 @@ def main():
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
 
+
 if __name__ == "__main__":
     main()
--- CarRentalSystem-CASESTUDY/util/db_conn_util.py
+++ CarRentalSystem-CASESTUDY/util/db_conn_util.py
@@ -1,9 +1,9 @@
 import mysql.connector
 from util.db_property_util import get_property_string
 
-
 def get_connection():
-    props = get_property_string('db.properties')
+    props = get_property_string(
+        r"C:\Users\sujan\Downloads\python-foundation-training-assignments\CarRentalSystem-CASESTUDY\db.properties")
 
     conn = mysql.connector.connect(
         host=props['host'],
